c123a0be011d2e8433785dc10ea6aef5cfffab6c
Andrew Batz <abatz@wealthforge.com>
2016-04-02 11:50:31 -0400

    adding element to the map

---
+++ map.js
@@ -0,0 +1,35 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW5kcmV3YmF0eiIsImEiOiJjaW1pajlhdXowMGNtdXlrdTNtaDByY3R3In0.aRKPHYn4_EDUrmcYxZ0Wgw';
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v8', //stylesheet location
+    center: [-74.50, 40], // starting position
+    zoom: 9 // starting zoom
+});
+map.on('load', function() {
+  map.addSource({
+    type: 'geojson',
+    data: {
+      "type": "Feature",
+      "geometry": {
+          "type": "Point",
+          "coordinates": [-77.03238901390978, 38.913188059745586]
+      },
+      "properties": {
+          "title": "Mapbox DC",
+          "marker-symbol": "monument"
+      }
+    }
+  });
+  map.addLayer({
+    "id": "markers",
+    "type": "symbol",
+    "source": "markers",
+    "layout": {
+        "icon-image": "{marker-symbol}-15",
+        "text-field": "{title}",
+        "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+        "text-offset": [0, 0.6],
+        "text-anchor": "top"
+    }
+  });  
+});
