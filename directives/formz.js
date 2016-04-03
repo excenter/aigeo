494fafd2afcdb2ade4ce581ef04317280894559a
Andrew Batz <abatz@wealthforge.com>
2016-04-02 21:33:15 -0400

    Y U no show?

--- directives/formz.js
+++ directives/formz.js
@@ -4,6 +4,7 @@ aigeo.directive('formz', [function() {
     restrict: 'E',
     templateUrl: '/directives/formz.html',
     link: (scope)=>{
+      console.log('fomrz is showing up');
       scope.text = 'bitch you guessed it';
       scope.report = function() {
         console.log('button Pressed');
