9e24c9d6838a3843210a99ee61be0242961c89a0
Andrew Batz <abatz@wealthforge.com>
2016-04-03 01:17:55 -0400

    testing scope and moving the index of the map

--- directives/app-body.js
+++ directives/app-body.js
@@ -4,11 +4,12 @@ aigeo.directive('appBody', [function() {
     restrict: 'E',
     templateUrl: '/directives/app-body.html',
     link: (scope)=>{
-      console.log('fomrz is showing up');
+      scope.winnipeg = 'I should show up';
       scope.text = 'bitch you guessed it';
       scope.report = function() {
         console.log('button Pressed');
       };
+      banan();
     }
   };
 }]);
