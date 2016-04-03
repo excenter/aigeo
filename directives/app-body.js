cc1aaf9964eea68e855a272d4c1c929e5a6b62cc
Andrew Batz <abatz@wealthforge.com>
2016-04-02 21:45:54 -0400

    cross propagate this bitch

--- directives/app-body.js
+++ directives/app-body.js
@@ -4,7 +4,11 @@ aigeo.directive('appBody', [function() {
     restrict: 'E',
     templateUrl: '/directives/app-body.html',
     link: (scope)=>{
+      console.log('fomrz is showing up');
       scope.text = 'bitch you guessed it';
+      scope.report = function() {
+        console.log('button Pressed');
+      };
     }
   };
 }]);
