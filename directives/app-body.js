f8cc2cbdd3af42e33c9202d41e39248d022fcd60
Andrew Batz <abatz@wealthforge.com>
2016-04-02 22:20:46 -0400

    and put it back

--- directives/app-body.js
+++ directives/app-body.js
@@ -10,34 +10,6 @@ aigeo.directive('appBody', [function() {
       scope.report = function() {
         console.log('button Pressed');
       };
-
-
-
-
     }
   };
 }]);
-L.mapbox.accessToken = 'pk.eyJ1IjoiYW5kcmV3YmF0eiIsImEiOiJjaW1pajlhdXowMGNtdXlrdTNtaDByY3R3In0.aRKPHYn4_EDUrmcYxZ0Wgw';
-var geojson = [
-  {
-    "type": "Feature",
-    "geometry": {
-      "type": "Point",
-      "coordinates": [-77.031952,38.913184]
-    }
-  },
-  {
-    "type": "Feature",
-    "geometry": {
-      "type": "Point",
-      "coordinates": [-122.413682,37.775408]
-    }
-  }
-];
-
-
-
-var mapGeo = L.mapbox.map('map_geo', 'mapbox.dark')
-  .setView([-122.413682,37.775408], 8);
-
-var myLayer = L.mapbox.featureLayer().setGeoJSON(geojson).addTo(mapGeo);
