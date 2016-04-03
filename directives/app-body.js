af6c8fd84d0c99497bc5370620f5a2bbabc9cbdd
Andrew Batz <abatz@wealthforge.com>
2016-04-03 09:59:13 -0400

    slightly less swearing

--- directives/app-body.js
+++ directives/app-body.js
@@ -4,8 +4,7 @@ aigeo.directive('appBody', [function() {
     restrict: 'E',
     templateUrl: '/directives/app-body.html',
     link: (scope)=>{
-      console.log('fomrz is showing up');
-      scope.text = 'bitch you guessed it';
+      scope.text = 'you know it';
       scope.report = function() {
         console.log('button Pressed');
       };
